fe03553479bca60e2f54f0a0cc4ed231604f1bcb
CCrisix <omgitzrllyu@gmail.com>
2026-09-12 00:17:21 -0400

    Add tensor shape and stride type aliases

---
+++ include/Types.hpp
@@ -0,0 +1,12 @@
+#ifndef TENSORLIB_TYPES_HPP
+#define TENSORLIB_TYPES_HPP
+
+#include <cstddef>
+#include <vector>
+
+namespace tensor {
+    using Strides = std::vector<size_t>;
+    using Shape = std::vector<size_t>;
+}
+
+#endif //TENSORLIB_TYPES_HPP
